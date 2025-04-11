d912a4967325e3f2027fd7bec961e3c54f89e09e
Jon Ator <jonathanator0@gmail.com>
2025-04-10 22:16:34 -0400

    update elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Swarm.MixProject do
     [
       app: :swarm,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
