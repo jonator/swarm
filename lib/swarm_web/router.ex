b73d1787839b02c4e449dc60369801a903f81364
Jon Ator <jonathanator0@gmail.com>
2025-04-20 22:06:08 -0400

    remove HTML

--- lib/swarm_web/router.ex
+++ lib/swarm_web/router.ex
@@ -6,7 +6,6 @@ defmodule SwarmWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, html: {SwarmWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
@@ -18,8 +17,7 @@ defmodule SwarmWeb.Router do
   scope "/", SwarmWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
-    oban_dashboard "/oban"
+    oban_dashboard("/oban")
   end
 
   # Other scopes may use custom stacks.
