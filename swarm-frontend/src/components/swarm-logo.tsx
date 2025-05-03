21329f4c61e4516b2e12d186c93acf4377cf6758
Jon Ator <jonathanator0@gmail.com>
2025-05-03 11:58:28 -0400

    add custom favicon

--- swarm-frontend/src/components/swarm-logo.tsx
+++ swarm-frontend/src/components/swarm-logo.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function SwarmLogo({
   size = 40,
   className = "",
@@ -5,38 +7,12 @@ export default function SwarmLogo({
   size?: number;
   className?: string;
 }) {
-  // Hexagon parameters
-  const center = size / 2;
-  const hexRadius = size * 0.45; // Slightly smaller than half to leave some margin
-
-  // Calculate points for hexagon shape
-  const hexPoints = [
-    [center, center - hexRadius], // top
-    [center + hexRadius * 0.866, center - hexRadius * 0.5], // top right
-    [center + hexRadius * 0.866, center + hexRadius * 0.5], // bottom right
-    [center, center + hexRadius], // bottom
-    [center - hexRadius * 0.866, center + hexRadius * 0.5], // bottom left
-    [center - hexRadius * 0.866, center - hexRadius * 0.5], // top left
-  ];
-
-  // Convert points array to SVG points string
-  const hexPointsString = hexPoints.map((point) => point.join(",")).join(" ");
-
   return (
     <div
       className={`relative ${className}`}
       style={{ width: size, height: size }}
     >
-      <svg
-        width={size}
-        height={size}
-        viewBox={`0 0 ${size} ${size}`}
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Simple white hexagon */}
-        <polygon points={hexPointsString} fill="white" />
-      </svg>
+      <Image src="/logo.svg" alt="Swarm Logo" width={32} height={32} />
     </div>
   );
 }
