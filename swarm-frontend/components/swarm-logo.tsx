a7ea552dbed70061d84568704b08786647bad478
Jon Ator <jonathanator0@gmail.com>
2025-05-04 12:24:59 -0400

    fix logo theming

--- swarm-frontend/components/swarm-logo.tsx
+++ swarm-frontend/components/swarm-logo.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+'use client'
 
 export default function SwarmLogo({
   size = 40,
@@ -9,10 +9,20 @@ export default function SwarmLogo({
 }) {
   return (
     <div
-      className={`relative ${className}`}
+      className={`relative text-foreground ${className}`}
       style={{ width: size, height: size }}
     >
-      <Image src='/logo.svg' alt='Swarm Logo' width={32} height={32} />
+      <svg
+        width={size}
+        height={size}
+        viewBox='0 0 32 32'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <polygon
+          points='16,1.5999999999999996 28.470399999999998,8.8 28.470399999999998,23.2 16,30.4 3.5296000000000003,23.2 3.5296000000000003,8.8'
+          fill='currentColor'
+        />
+      </svg>
     </div>
   )
 }
